c1ce5b38cc44f2a56b892b0725db39a10526b99c
Edwin <edwin.jared@hotmail.com>
2021-10-06 12:20:35 -0500

    Primer comit

---
+++ funciones.js
@@ -0,0 +1,17 @@
+// Declarativas
+function miFuncion()
+{
+return 3;
+}
+miFuncion();
+
+//Expresión anonimas  // Parametros
+var miFuncion = function(a,b)
+{
+return a+b;
+}
+miFuncion();
+
+function saludar (nombre) {
+    console.log("Hola " + nombre)
+    } ;
---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre="Jared";
+
+//declarar 
+var edad;
+
+edad
+//inicializar
+edad=30;
+
+edad                 // INDICE []
+                    //0         //1
+  // Arreglo
+ console.log(elementos[0]);
+var persona = {
+    nombres: "Diego",
+    edad: 30
+} // objeto
+ console.log(persona.nombres);
+persona;
